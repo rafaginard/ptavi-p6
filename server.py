3a1cf685689b9afa46cb2be28c24706745472e83
Rafael Ginard Aguilera <rginard@gamma05.aulas.gsyc.urjc.es>
2017-11-20 15:48:09 +0100

    Server con bugs

--- server.py
+++ server.py
@@ -4,6 +4,7 @@
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
+import os
 import socketserver
 import sys
 
@@ -17,17 +18,38 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print(line.decode('utf-8'))
-            DATA = line.decode('utf-8').split(" ")
-            if DATA[0] == "INVITE":
-                self.wfile.write(b"SIP/2.0 100 Trying\nSIP/2.0 180 Ringing\nSIP/2.0 200 OK")
-
-            # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            else:
+                Check_Data = False
+                print(line.decode('utf-8'))
+                DATA = line.decode('utf-8').split(" ")
+                print(len(DATA))
+                if len(DATA) == 3:
+                    if DATA[1].find(" sip:") and DATA[2] == (" SIP/2.0"):
+                        Check_Data = True
+                if DATA[0] == "INVITE" and Check_Data:
+                    self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+                    self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                elif DATA[0] == "BYE" and Check_Data:
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                elif DATA[0] == "ACK" and Check_Data:
+                    fichero_audio = sys.argv[3]
+                    aEjecutar = "mp32rtp -i 127.0.0.1 -p 23032 < " + fichero_audio
+                    os.system(aEjecutar)
+                elif DATA[0] != ("INVITE" or "ACK" or "BYE") and Check_Data:
+                    self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+                else:
+                    self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+                # Si no hay más líneas salimos del bucle infinito
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer((sys.argv[1], int(sys.argv[2])), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    try:
+        fichero_audio = sys.argv[3]
+        print("Lanzando servidor UDP de eco...")
+        serv.serve_forever()
+    except:
+        sys.exit("Usage: python3 server.py IP port audio_file")
