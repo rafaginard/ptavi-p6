df22a655157a323ae644188f0cb9ec30a6b393d3
Rafael Ginard Aguilera <rginard@gamma08.aulas.gsyc.urjc.es>
2017-11-20 10:37:52 +0100

    Cliente recibe 200 OK

--- client.py
+++ client.py
@@ -5,26 +5,33 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
-
+METHOD = ""
+SIP_Data = []
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+LINE = 'INVITE sip:'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+    METHOD = sys.argv[1]
+    Message = sys.argv[2]
+    SIP_Data = Message.replace(":","@").split("@")
+    User_Name = SIP_Data[0]
+    Server_Ip = SIP_Data[1]
+    Server_Port = SIP_Data[2]
+    if METHOD == "INVITE":
+        my_socket.connect((Server_Ip, int(Server_Port)))
+        my_socket.send(bytes(LINE + Message, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+    elif METHOD == "BYE":
+        my_socket.send(bytes("BYE sip:" + Message , 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+    print("Enviando: " + Message)
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
-
+    print(data.decode('utf-8'))
 print("Fin.")
